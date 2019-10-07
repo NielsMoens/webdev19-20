236b4b3ae0fdd35f8f4e3671140e238f6c37f1d2
Niels Moens <49305745+NielsMoens@users.noreply.github.com>
2019-10-07 20:43:04 +0200

    Update lotto.php

--- week2/lotto trekking/lotto.php
+++ week2/lotto trekking/lotto.php
@@ -1,3 +1,6 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,18 +11,27 @@
 </head>
 <body>
     <?php
+    
     session_start();
-    if(!isset($_SESSION['index']))
-        $_SESSION['index'] = rand(1,100);
-      
-        for($i = 0; $i < 20; $i++){
-            
-            for($i = 0; $i < 20; $i++){
-            echo '<h1>"'.$_SESSION['index'].'"</h1>';
-            }
-        }
 
-       
+    if(!isset($_SESSION['index'])) {
+        $_SESSION['index'] = [];
+        $_SESSION['numbers'] = range(0, 45);
+        shuffle($_SESSION['numbers']);
+        
+        $randomElement = array_rand($_SESSION['numbers']); 
+        array_push($_SESSION['index'], $randomElement);
+        unset($_SESSION['numbers'][$randomElement]);
+         
+    } else if (sizeof($_SESSION['index']) < 6) { 
+        $randomElement = array_rand($_SESSION['numbers']); 
+        array_push($_SESSION['index'], $randomElement);
+        unset($_SESSION['numbers'][$randomElement]);
+    }
+
+    foreach ($_SESSION['index'] as $element)
+        echo '<h1>"'.$element.'"</h1>';
+    
     ?>
 </body>
 </html>
